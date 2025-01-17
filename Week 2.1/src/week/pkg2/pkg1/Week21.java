f5a280ba9e1fcf91795ea5aefa4c5885939fd45a
taranjeetsinghsin16578 <sin16578@sheridancollege.ca>
2025-01-17 15:52:00 -0500

    week 2 commit

---
+++ Week 2.1/src/week/pkg2/pkg1/Week21.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week.pkg2.pkg1;
+
+/**
+ *
+ * @author taran
+ */
+public class Week21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
